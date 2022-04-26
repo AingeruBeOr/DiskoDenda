f3d60be681445795f25b62230f24313b214241f0
alaia <alaia@LAPTOP-DH5KA772>
2022-04-26 15:14:51 +0200

    KCENO

--- src/disko/DB.java
+++ src/disko/DB.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.*;
-
+//PRUEBA ALAIA
 public class DB {
     private BufferedReader br;
     private Connection konexioa;
